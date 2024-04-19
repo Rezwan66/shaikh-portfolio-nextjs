bda9b4eae8741ed9534177d28db4778718f2f348
Rezwan66 <shaikhrezwan66@gmail.com>
2024-04-19 23:33:46 +0200

    added a Next Image of my profile with a div with bg and buttons for learn more and resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const HeroSection = () => {
   return (
     <section>
@@ -10,15 +12,32 @@ const HeroSection = () => {
           <p className="text-[#ADB7BE] mb-6 text-lg lg:text-xl tracking-wider font-bold">
             Developer by Passion, Engineer by Mind!
           </p>
-          <p className="text-white lg:text-lg">
+          <p className="text-white lg:text-lg mb-4">
             Aspiring MERN Stack Developer and Digital Engineering graduate
-            student, seeking a full-time junior web developer position or
-            internship. Eager on building effective and creative user interfaces
+            student, eager on building effective and creative user interfaces
             with React, Next.js, HTML5, CSS3, JavaScript, Express and MongoDB.
           </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              Learn More
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
+              Download Resume
+            </button>
+          </div>
         </div>
         {/* image */}
-        <div className="col-span-5"></div>
+        <div className="col-span-5 place-self-center mt-6 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
+            <Image
+              src="/images/noBgProfile.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={500}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
