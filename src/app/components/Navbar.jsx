b00ab064ffd1fb03ad37f5cbac11b231de029e89
Rezwan66 <shaikhrezwan66@gmail.com>
2024-04-22 18:41:13 +0200

    created a new nav link component but not woerking, need to look again

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,20 @@
 import Link from 'next/link';
+import NavLink from './NavLink';
+
+const navLinks = [
+  {
+    title: 'About',
+    path: '#about',
+  },
+  {
+    title: 'Projects',
+    path: '#projects',
+  },
+  {
+    title: 'Contact',
+    path: '#contact',
+  },
+];
 
 const Navbar = () => {
   return (
@@ -7,16 +23,13 @@ const Navbar = () => {
         <Link href={'/'} className="text-5xl text-white font-semibold">
           Logo
         </Link>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul>
-            <li>
-              <Link
-                href={'#about'}
-                className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-              >
-                About
-              </Link>
-            </li>
+        <div className="menu block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 flex-row">
+            {navLinks.map((link, index) => {
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>;
+            })}
           </ul>
         </div>
       </div>
