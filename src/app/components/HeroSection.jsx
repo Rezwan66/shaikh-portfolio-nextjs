4a4acbd333d83505b8450d1e2918a162417c9ee3
Rezwan66 <shaikhrezwan66@gmail.com>
2024-04-22 18:23:50 +0200

    created a Navbar component with logo and menu item for the navigation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                'Shaikh',
+                'Shaikh Rezwan',
                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                 'Web Developer',
                 1000,
