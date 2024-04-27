0e2fa7a1986cab7e9c1aadef188bc33799688aa7
Rezwan66 <shaikhrezwan66@gmail.com>
2024-04-27 23:53:38 +0200

    fixed the navbar with a bg opacity

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import NavLink from './NavLink';
 import Image from 'next/image';
+import NavLink from './NavLink';
 
 const navLinks = [
   {
@@ -19,9 +19,12 @@ const navLinks = [
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href={'/'} className="text-5xl text-white font-semibold">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+        <Link
+          href={'/'}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
           <Image
             alt="website logo"
             src="/images/logo_web.png"
@@ -30,12 +33,12 @@ const Navbar = () => {
           />
         </Link>
         <div className="menu block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 flex-row">
-            {navLinks.map((link, index) => {
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title} />
-              </li>;
-            })}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
