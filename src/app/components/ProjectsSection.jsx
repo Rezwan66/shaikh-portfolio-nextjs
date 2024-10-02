5ff07e5181f89613e5d63c1faec3d839658ae6c3
Rezwan66 <shaikhrezwan66@gmail.com>
2024-10-02 23:09:30 +0200

    added group hover functionality to the projects card

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,8 +1,87 @@
+import ProjectCard from './ProjectCard';
+
+const projectsData = [
+  {
+    id: 1,
+    title: 'React Portfolio Website',
+    description: 'Project 1 description',
+    image: '/images/projects/explore-nest.jpg',
+    tag: ['All', 'Web'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+  {
+    id: 2,
+    title: 'Potography Portfolio Website',
+    description: 'Project 2 description',
+    image: '/images/projects/auto-maniac.jpg',
+    tag: ['All', 'Web'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+  {
+    id: 3,
+    title: 'E-commerce Application',
+    description: 'Project 3 description',
+    image: '/images/projects/travel-buddy.jpg',
+    tag: ['All', 'Web'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+  {
+    id: 4,
+    title: 'Food Ordering Application',
+    description: 'Project 4 description',
+    image: '/images/projects/task-master.jpg',
+    tag: ['All', 'Mobile'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+  {
+    id: 5,
+    title: 'React Firebase Template',
+    description: 'Authentication and CRUD operations',
+    image: '/images/projects/entertainment-freak.jpg',
+    tag: ['All', 'Web'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+  {
+    id: 6,
+    title: 'Full-stack Roadmap',
+    description: 'Project 5 description',
+    image: '/images/projects/bistro-boss.jpg',
+    tag: ['All', 'Web'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+  {
+    id: 7,
+    title: 'Stay Vista',
+    description: 'Project 5 description',
+    image: '/images/projects/stay-vista.jpg',
+    tag: ['All', 'Web'],
+    gitUrl: '/',
+    previewUrl: '/',
+  },
+];
+
 const ProjectsSection = () => {
   return (
     <>
-      <div>
-        <h2> Here we will map our projects </h2>
+      <h2 className="text-center text-4xl font-bold text-white mt-4">
+        {' '}
+        My Projects{' '}
+      </h2>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {projectsData?.map(project => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+          />
+        ))}
       </div>
     </>
   );
