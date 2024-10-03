2160f8c867633b058e9c9cb40ed484527eb7510b
Rezwan66 <shaikhrezwan66@gmail.com>
2024-10-03 21:27:23 +0200

    created project tag button component instaed of repeat

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -1,10 +1,14 @@
-const ProjectTag = () => {
+const ProjectTag = ({ name, onClick, isSelected }) => {
+  const buttonStyles = isSelected
+    ? 'text-white bg-yellow-500'
+    : 'text-[#ADB7BE] border-slate-600 hover:border-white';
   return (
-    <>
-      <div>
-        <h2> HELLO I Am ProjectTag </h2>
-      </div>
-    </>
+    <button
+      onClick={onClick}
+      className={`${buttonStyles} rounded-full border-2 px-4 py-2 text-xl cursor-pointer`}
+    >
+      {name}
+    </button>
   );
 };
 export default ProjectTag;
