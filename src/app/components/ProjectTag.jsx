63316d2501cef2fccd953eb14faf8261f3bb394e
Rezwan66 <shaikhrezwan66@gmail.com>
2024-10-03 21:56:55 +0200

    completed the project tags section and projects filtering option by tag

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -1,10 +1,10 @@
 const ProjectTag = ({ name, onClick, isSelected }) => {
   const buttonStyles = isSelected
-    ? 'text-white bg-yellow-500'
+    ? 'text-white border-yellow-500'
     : 'text-[#ADB7BE] border-slate-600 hover:border-white';
   return (
     <button
-      onClick={onClick}
+      onClick={() => onClick(name)}
       className={`${buttonStyles} rounded-full border-2 px-4 py-2 text-xl cursor-pointer`}
     >
       {name}
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,57 +1,60 @@
+'use client';
+import { useState } from 'react';
 import ProjectCard from './ProjectCard';
+import ProjectTag from './ProjectTag';
 
 const projectsData = [
   {
     id: 1,
-    title: 'React Portfolio Website',
+    title: 'Explore Nest',
     description: 'Project 1 description',
     image: '/images/projects/explore-nest.jpg',
-    tag: ['All', 'Web'],
+    tag: ['All', 'Travel & Tourism'],
     gitUrl: '/',
     previewUrl: '/',
   },
   {
     id: 2,
-    title: 'Potography Portfolio Website',
+    title: 'Auto Maniac',
     description: 'Project 2 description',
     image: '/images/projects/auto-maniac.jpg',
-    tag: ['All', 'Web'],
+    tag: ['All', 'E-commerce'],
     gitUrl: '/',
     previewUrl: '/',
   },
   {
     id: 3,
-    title: 'E-commerce Application',
+    title: 'Travel Buddy',
     description: 'Project 3 description',
     image: '/images/projects/travel-buddy.jpg',
-    tag: ['All', 'Web'],
+    tag: ['All', 'Travel & Tourism'],
     gitUrl: '/',
     previewUrl: '/',
   },
   {
     id: 4,
-    title: 'Food Ordering Application',
+    title: 'Task Master',
     description: 'Project 4 description',
     image: '/images/projects/task-master.jpg',
-    tag: ['All', 'Mobile'],
+    tag: ['All', 'Task Management'],
     gitUrl: '/',
     previewUrl: '/',
   },
   {
     id: 5,
-    title: 'React Firebase Template',
+    title: 'Entertainment Freak',
     description: 'Authentication and CRUD operations',
     image: '/images/projects/entertainment-freak.jpg',
-    tag: ['All', 'Web'],
+    tag: ['All', 'Events Management'],
     gitUrl: '/',
     previewUrl: '/',
   },
   {
     id: 6,
-    title: 'Full-stack Roadmap',
+    title: 'Bistro Boss',
     description: 'Project 5 description',
     image: '/images/projects/bistro-boss.jpg',
-    tag: ['All', 'Web'],
+    tag: ['All', 'E-commerce'],
     gitUrl: '/',
     previewUrl: '/',
   },
@@ -60,13 +63,20 @@ const projectsData = [
     title: 'Stay Vista',
     description: 'Project 5 description',
     image: '/images/projects/stay-vista.jpg',
-    tag: ['All', 'Web'],
+    tag: ['All', 'Travel & Tourism'],
     gitUrl: '/',
     previewUrl: '/',
   },
 ];
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState('All');
+  const handleTagChange = newTag => {
+    setTag(newTag);
+  };
+  const filteredProjects = projectsData?.filter(project =>
+    project.tag.includes(tag)
+  );
   return (
     <>
       {/* title */}
@@ -74,11 +84,34 @@ const ProjectsSection = () => {
         {' '}
         My Projects{' '}
       </h2>
+      {/* project tags selector */}
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <button className="rounded-full border-2 border-yellow-500 px-4 py-2 text-xl cursor-pointer">
-          All
-        </button>
-        <button className="rounded-full border-2 border-slate-500 hover:border-white px-4 py-2 text-xl cursor-pointer">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === 'All'}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Travel & Tourism"
+          isSelected={tag === 'Travel & Tourism'}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Task Management"
+          isSelected={tag === 'Task Management'}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="E-commerce"
+          isSelected={tag === 'E-commerce'}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Events Management"
+          isSelected={tag === 'Events Management'}
+        />
+        {/* <button className="rounded-full border-2 border-slate-500 hover:border-white px-4 py-2 text-xl cursor-pointer">
           Travel & Tourism
         </button>
         <button className="rounded-full border-2 border-slate-500 hover:border-white px-4 py-2 text-xl cursor-pointer">
@@ -89,11 +122,11 @@ const ProjectsSection = () => {
         </button>
         <button className="rounded-full border-2 border-slate-500 hover:border-white px-4 py-2 text-xl cursor-pointer">
           Events Management
-        </button>
+        </button> */}
       </div>
       {/* project cards */}
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projectsData?.map(project => (
+        {filteredProjects?.map(project => (
           <ProjectCard
             key={project.id}
             title={project.title}
