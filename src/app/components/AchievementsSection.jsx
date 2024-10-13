2db7367f9894d932a807138ba2dc6eb4433a64d6
Rezwan66 <shaikhrezwan66@gmail.com>
2024-10-13 22:07:09 +0200

    fixed the responsiveness in the achievement section

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -33,7 +33,7 @@ const achievementsList = [
 const AchievementsSection = () => {
   return (
     <section className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
+      <div className="border-[#33353F] border rounded-md py-8 px-16 flex md:flex-row flex-col gap-4 md:gap-0 items-center justify-between">
         {achievementsList.map((achievement, idx) => {
           return (
             <div
