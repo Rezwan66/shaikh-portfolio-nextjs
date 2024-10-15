e074172893ccec5145522a1c8a68c6bfdbcc8151
Rezwan66 <shaikhrezwan66@gmail.com>
2024-10-15 23:36:49 +0200

    added a scroll to top button on the footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,15 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { animateScroll } from 'react-scroll';
 
 const Footer = () => {
+  const scrollToTop = () => {
+    animateScroll.scrollToTop();
+  };
   return (
-    <footer className="footer z-10 border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between items-center">
+    <footer className="footer z-10 border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
+      <div className="p-12 flex justify-between items-center">
         {/* logo */}
         <Link href={'/'} className="text-2xl md:text-5xl font-semibold">
           <Image
@@ -18,6 +23,12 @@ const Footer = () => {
         <p className="text-slate-400">
           © Shaikh Rezwan {new Date().getFullYear()}. All rights reserved.
         </p>
+        <button
+          className="button py-2 px-4 border border-slate-400 rounded-full text-2xl hover:border-primary-500 text-primary-500"
+          onClick={scrollToTop}
+        >
+          ⬆
+        </button>
       </div>
     </footer>
   );
