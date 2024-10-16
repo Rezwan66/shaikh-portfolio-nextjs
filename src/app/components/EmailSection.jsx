b8fb7baecb04dc1d2740b7c39c2e3dc749f2c79f
Rezwan66 <shaikhrezwan66@gmail.com>
2024-10-16 11:49:26 +0200

    added padding to email sectuion

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,7 +8,7 @@ const EmailSection = () => {
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      className="grid md:grid-cols-2 my-12 md:my-12 md:py-32 gap-4 relative"
     >
       {/* mash */}
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))]  from-primary-600 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
